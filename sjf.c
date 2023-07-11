0656e19b56c97832a93a29decc7001137e6cbb0d
Babithrai <119489654+Babithrai@users.noreply.github.com>
2023-07-11 09:44:57 +0000

    sjf.c

--- sjf.c
+++ sjf.c
@@ -1,78 +1,70 @@
 #include<stdio.h>
-void printer(int processes[],int n,int burst_time[],int arrival_time[])
+ int main()
 {
-int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n];
-for (int i = 0; i < n; i++)//calculating times
-{
-if(i==0)
-{
-WT[0]=0;
-completion_time[0]=burst_time[0]+arrival_time[0];
-TAT[0]=completion_time[0]-arrival_time[0];
-total_TAT=TAT[0];
-continue;
+    int burst_time[20],p[20],waiting_time[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_waiting_time,avg_tat;
+    printf("please enter number of process: ");
+    scanf("%d",&n);
+    printf("\n enter the Burst Time:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&burst_time[i]);
+        p[i]=i+1;        
+    }
+   // from here, burst times sorted
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(burst_time[j]<burst_time[pos])
+                pos=j;
+        }
+        temp=burst_time[i];
+        burst_time[i]=burst_time[pos];
+        burst_time[pos]=temp;
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+    waiting_time[0]=0;            
+    for(i=1;i<n;i++)
+    {
+        waiting_time[i]=0;
+        for(j=0;j<i;j++)
+            waiting_time[i]+=burst_time[j];
+        total+=waiting_time[i];
+    }
+    avg_waiting_time=(float)total/n;      
+    total=0;
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=burst_time[i]+waiting_time[i];  
+        total+=tat[i];
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",p[i],burst_time[i],waiting_time[i],tat[i]);
+    }
+    avg_tat=(float)total/n;    
+    printf("\n\n the average Waiting Time=%f",avg_waiting_time);
+    printf("\n  the average Turnaround Time=%f\n",avg_tat);
 }
-if(completion_time[i-1]<arrival_time[i])
-{
-completion_time[i]=arrival_time[i]+burst_time[i];
-}
-else
-{
-completion_time[i]=completion_time[i-1]+burst_time[i];
-}
-TAT[i]=completion_time[i]-arrival_time[i];
-total_TAT+=TAT[i];
-WT[i]=TAT[i]-burst_time[i];
-total_WT+=WT[i];
-}
-printf("Processes\tAT\tBT\tWT\tTAT\tCT\n");
-for (int i = 0; i < n; i++)
-{
-printf("%d\t\t%d\t%d\t%d\t%d\t%d\n",processes[i],arrival_time[i],burst_time[i],WT[i],TAT[i
-],completion_time[i]);
-}
-printf("AVG waiting time: %2f\n",(float)total_WT/n);
-printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
-printf("\n");
-}
-void sjf(int processes[],int n,int burst_time[],int arrival_time[])
-{
-int WT[n],TAT[n],total_WT=0,completion_time[n],total_TAT=0;
-for (int i = 0; i < n; i++)
-{
-int shortest_job_index=i;
-for (int j = i+1; j < n; j++)
-{
-if (burst_time[j]<burst_time[shortest_job_index])
-{
-shortest_job_index=j;
-}
-}
-int temp=burst_time[i];
-burst_time[i]=burst_time[shortest_job_index];
-burst_time[shortest_job_index]=temp;
-temp=processes[i];
-processes[i]=processes[shortest_job_index];
-processes[shortest_job_index]=temp;
-temp=arrival_time[i];
-arrival_time[i]=arrival_time[shortest_job_index];
-arrival_time[shortest_job_index]=temp;
-}
-printf("SJF agorithm\n");
-printer(processes,n,burst_time,arrival_time); 
-} void main() 
-{ 
-int n; int quantum;
-printf("Enter the number of processes: ");
-scanf("%d",&n);
-int processes[n],burst_time[n],arrival_time[n];
-for (int i = 0; i < n; i++) 
-{ 
-printf("Enter the arrival time for process %d:",i+1);
-scanf("%d",&arrival_time[i]);
-printf("Enter the Burst time for processes %d:",i+1);
-scanf("%d",&burst_time[i]); 
-processes[i]=i+1;
-} 
-sjf(processes,n,burst_time,arrival_time); 
-} 
+
+
+
+/*OUTPUT:please enter number of process: 4
+
+ enter the Burst Time:
+p1:5
+p2:4
+p3:2
+p4:1
+
+Process     Burst Time          Waiting Time    Turnaround Time
+p4                1                 0                   1
+p3                2                 1                   3
+p2                4                 3                   7
+p1                5                 7                   12
+
+ the average Waiting Time=2.750000
+  the average Turnaround Time=5.750000*/
